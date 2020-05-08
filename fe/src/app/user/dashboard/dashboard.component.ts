756bc30f5c952087bb96548475f9f044888f6cb1
Bao PT <phutuongbao1999@gmail.com>
2020-05-08 12:01:01 +0700

    Add Observable

--- fe/src/app/user/dashboard/dashboard.component.ts
+++ fe/src/app/user/dashboard/dashboard.component.ts
@@ -1,68 +1,59 @@
-import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError, of, Subscription } from 'rxjs';
 import { UserDefineService } from './../user-define.service';
 import { UserService } from './../user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit, OnDestroy {
+  subscription: Subscription[] = [];
   constructor(
     private userSerive: UserService,
     private userDefineService: UserDefineService
   ) { }
 
   ngOnInit(): void {
-    // this.getUserInfo();
-    const observable = new Observable<any>(ob => {
-      ob.next(this.getId());
+    this.observableApi();
+  }
+
+  ngOnDestroy() {
+    this.subscription.forEach(i => {
+      i.unsubscribe();
+    })
+  }
+
+  createObservable() {
+    return new Observable<any>(ob => {
+      ob.next(this.getIdUser());
       ob.complete();
     });
-    observable.subscribe(val => {
-      this.getUser(val).subscribe(res => {
-        console.log(res);
-      }, err => {
-        console.log(err);
-      })
-    }, err => {
-      console.log(err);
-    }, () => {
-      console.log('Observable Subscribe Done => Call Api Get User Infomation');
-    });
   }
 
-  getId() {
-    return this.userDefineService.getIdUser();
-  }
-
-  getUser(id) {
-    return this.userSerive.getInfoUser(id);
-  }
-
-  getIdUser() {
-    return new Promise((resolve, reject) => {
-      const id = this.userDefineService.getIdUser();
-      if (id) {
-        resolve(id)
-      } else {
-        reject(null);
-      }
-    });
-  }
-
-  async getUserInfo() {
-    await this.getIdUser().then(
-      res => {
-        this.userSerive.getInfoUser(res).subscribe(res => {
+  observableApi() {
+    this.subscription.push(
+      this.createObservable().subscribe({
+        next: value => this.getInfoUser(value).subscribe(res => {
           console.log(res);
         }, err => {
           console.log(err);
-        });
-      }
+        }),
+        error: err => console.log(err),
+        complete: () => console.log('Observer Done')
+      })
     )
+  }
+
+  getIdUser() {
+    return this.userDefineService.getIdUser();
+  }
+
+  getInfoUser(id) {
+    console.log(id);
+    return this.userSerive.getInfoUser(id);
   }
 
 }
