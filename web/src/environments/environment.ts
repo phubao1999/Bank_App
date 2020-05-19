422ffe656fd8d9d07ad3e2d1dee34fa1fafc1628
Bao PT <phutuongbao1999@gmail.com>
2020-05-19 09:48:12 +0700

    Setup Server Connect

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_SERVER: 'http://localhost:8080',
+  SOCKET_SERVER: 'http://localhost:3000'
 };
 
 /*
