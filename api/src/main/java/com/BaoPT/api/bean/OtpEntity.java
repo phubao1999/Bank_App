310df0136a18bc59f7cd097be92be8d3a14fea11
Bao PT <phutuongbao1999@gmail.com>
2020-05-19 17:59:01 +0700

    Add Otp Entity

---
+++ api/src/main/java/com/BaoPT/api/bean/OtpEntity.java
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// © 2020 VNEXT TRAINING
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package com.BaoPT.api.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * [OVERVIEW] XXXXX.
+ *
+ * @author: (VNEXT) BaoPT
+ * @version: 1.0
+ * @History
+ * [NUMBER]  [VER]     [DATE]          [USER]             [CONTENT]
+ * --------------------------------------------------------------------------
+ * 001       1.0       2020/05/19      (VNEXT) BaoPT       Create new
+*/
+
+@Entity
+@Table(name = "otp")
+public class OtpEntity {
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+    public Integer getOtpNumber() {
+        return otpNumber;
+    }
+
+    public void setOtpNumber(Integer otpNumber) {
+        this.otpNumber = otpNumber;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "id_user")
+    private Integer idUser;
+
+    @Column(name = "otp_number")
+    private Integer otpNumber;
+}
